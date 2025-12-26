47d7965984d080cbd48c1a9d34836f8e36142343
Morteza Bahal <kheilibahali@gmail.com>
2025-12-26 11:24:39 -0500

    chore: init repo + postgres + db sanity

---
+++ sanity/db_check.py
@@ -0,0 +1,11 @@
+﻿import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.environ["DATABASE_URL"]
+with psycopg2.connect(url) as conn:
+    with conn.cursor() as cur:
+        cur.execute("SELECT now();")
+        print("DB OK:", cur.fetchone()[0])
